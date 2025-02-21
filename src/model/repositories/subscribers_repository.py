34e3f7e73a06e52d8d109ab86a5e5a1488d75f9a
akillez01 <achilles.oliveira.souza@gmail.com>
2025-02-21 10:41:17 -0500

    feat: adding new methods in subscribers_repo

--- src/model/repositories/subscribers_repository.py
+++ src/model/repositories/subscribers_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func, desc
 from src.model.configs.connection import DBConnectionHandler
 from src.model.entities.inscritos import Inscritos
 from .interfaces.subscribers_repository import SubscribersRepositoryInterface
@@ -18,12 +19,45 @@ class SubscribersRepository(SubscribersRepositoryInterface):
                 db.session.rollback()
                 raise exception
 
-    def select_subscriber(self, subscriber_email: str, evento_id: int) -> Inscritos:
+    def select_subscriber(self, email: str, evento_id: int) -> Inscritos:
         with DBConnectionHandler() as db:
             data = (
                 db.session
                 .query(Inscritos)
-                .filter(Inscritos.email == subscriber_email, Inscritos.evento_id == evento_id)
+                .filter(
+                    Inscritos.email == email,
+                    Inscritos.evento_id == evento_id
+                )
                 .one_or_none()
             )
             return data
+        
+    def select_subscriber_by_link(self, link: str, event_id: int) -> list:
+        with DBConnectionHandler() as db:
+            data = (
+                db.session
+                .query(Inscritos)
+                .filter(
+                    Inscritos.link == link,
+                    Inscritos.evento_id == event_id
+                )
+                .all()
+            )
+            return data
+    def get_ranking(self, event_id: int) -> list:
+        with DBConnectionHandler() as db:
+            result = (
+                db.session
+                .query(
+                    Inscritos.link,
+                    func.count(Inscritos.id).label("total")
+                )
+                .filter(
+                    Inscritos.evento_id == event_id,
+                    Inscritos.link.isnot(None)
+                )
+                .group_by(Inscritos.link)
+                .order_by(desc("total"))
+                .all()
+            )
+            return result
--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -12,14 +12,23 @@ def test_insert():
     subs_repo = SubscribersRepository(db_url)
     subs_repo.insert(subscriber_info)
     
-@pytest.mark.skip("Insert in db")
+@pytest.mark.skip("Insert in DB")
 def test_select_subscriber():
     email = "email22@email.com"
     evento_id = 2
 
     db_url = "sqlite:///schema.db"  # Adicionado
-    subs_repo = SubscribersRepository(db_url)
+    subs_repo = SubscribersRepository()
 
     # Tenta chamar o método
     resp = subs_repo.select_subscriber(email, evento_id)
     print(resp.nome)
+    
+def test_ranking():
+    evento_id = 8
+    subs_repo = SubscribersRepository()
+    resp = subs_repo.get_ranking(evento_id)
+    print()
+    
+    for elem in resp:
+        print(f"Link: {elem.link}, total de inscritos: {elem.total}")
